9cd52dac478342392faae0b4e1bb02298dd5d4f5
ZizoNaser <zizo_naser@ymail.com>
2019-05-22 16:26:28 +0200

    Sync the Form code with its HTML

--- src/app/form-dd/form-dd.component.ts
+++ src/app/form-dd/form-dd.component.ts
@@ -25,7 +25,10 @@ export class FormDdComponent implements OnInit {
       'zip'      : new FormControl(),
       'gender'   : new FormControl(),
     });
-    
+  }
+
+  onSubmit(){
+    console.log(this.myForm);
   }
 
 }
